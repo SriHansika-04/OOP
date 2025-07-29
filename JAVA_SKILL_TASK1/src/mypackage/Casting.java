92c27e0146fdef57078aca5cd40d694b6a045e4b
SriHansika-04 <2420030556@klh.edu.in>
2025-07-29 14:44:13 +0530

    Add files via upload

---
+++ JAVA_SKILL_TASK1/src/mypackage/Casting.java
@@ -0,0 +1,19 @@
+package mypackage;
+
+public class Casting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		  
+		        double value = 10.75;
+		        int result = (int) value;  // manually cast double to int
+		        System.out.println("Original double: " + value);
+		        System.out.println("After casting to int: " + result);
+		    }
+		}
+
+
+	
+
+
